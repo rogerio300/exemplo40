b6c3bf9fa6141cfea0c1f9f27205907ad6ea89a4
Rogerio Souza <rg.nascimeto.souza@gmail.com>
2020-11-04 16:05:55 -0300

    instalando tabela usuario

---
+++ install.php
@@ -0,0 +1,23 @@
+<?php
+require("db.php");
+
+banco("DROP TABLE IF EXISTS usuario");
+echo "<p>DB: Tabela usuario apagada...</P>";
+
+$sql ="CREATE TABLE usuario (";
+$sql .="codigo SERIAL NOT NULL,";
+$sql .="nome VARCHAR(250),";
+$sql .="email VARCHAR(100),";
+$sql .="senha VARCHAR(100),";
+$sql .="PRIMARY KEY (codigo)";
+$sql .=");";
+banco($sql);
+echo "<p>DB: Tabela usuario criada...</p>";
+
+
+
+
+
+
+
+?>
